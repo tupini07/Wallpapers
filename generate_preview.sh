6bce51c56bdbeb48256e7653f6486970d2d9a780
Andrea Tupini <atupini@cecropiasolutions.com>
2020-02-26 14:17:38 -0600

    when generating preview, update git repo with upstream

--- generate_preview.sh
+++ generate_preview.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # Generate a Preview.md file with links and 
-# previews of all the images in the repo
+# previews of all the images in the repo and update repo
+
+git pull git@github.com:turing753/wallpapers.git master
 
 # get all files that don't end in `md` or `sh` and for each 
 # echo to `Preview.md` the appropriate line to include the image
